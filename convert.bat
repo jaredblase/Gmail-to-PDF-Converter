deb4b6ec179122b545c118f7509bc46fec17f0bc
jaredblase <jaredblase@gmail.com>
2021-04-07 00:23:51 +0800

    add: batch file for easier execution

---
+++ convert.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+:: This batch file runs the convert.py script
+TITLE Gmail to PDF Converter
+ECHO ----------------------------------------
+ECHO Starting program...
+ECHO ----------------------------------------
+ECHO.
+
+python src/convert.py
+
+ECHO Conversion finished!
+ECHO.
+PAUSE
+
